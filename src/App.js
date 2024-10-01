df4bde67c1f0791f7ed4124acff4c05b50e0055d
jamilahmad11 <jamiljj01970@gmail.com>
2024-09-30 23:26:37 -0700

    sdf

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PotPolio from './component/PotPolio'
+
+const App = () => {
+  return (
+    <div className=''>
+      <PotPolio/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/PotPolio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import"./style"
+const PotPolio = () => {
+  return (
+    <div className='aa'>PotPolio</div>
+  )
+}
+
+export default PotPolio
